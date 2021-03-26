ed1d694cd7732f97bf701c4bc9ba00ffd42d341b
Sebastian Strzalkowski <sebastianstrzalkowski.strzala@gmail.com>
2021-03-26 21:59:19 +0100

    Add jedis pool.

--- src/main/java/pl/sebastianstrzalkowski/gasbackend/service/GasService.java
+++ src/main/java/pl/sebastianstrzalkowski/gasbackend/service/GasService.java
@@ -8,7 +8,17 @@ import pl.sebastianstrzalkowski.gasbackend.entities.GasPriceRepository;
 import pl.sebastianstrzalkowski.gasbackend.models.GasPrice;
 import pl.sebastianstrzalkowski.gasbackend.models.Result;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.net.URI;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.logging.Logger;
 
 @Service
@@ -19,10 +29,10 @@ public class GasService {
 
 
 //    private String url = System.getenv("REDISCLOUD_URL");
-    private Jedis jedis = new Jedis();
+//    private Jedis jedis = new Jedis();
 
     public Result getGasPrice(){
-
+        Jedis jedis = getPool().getResource();
         String fastPrice = jedis.get("price:fast");
         String safePrice = jedis.get("price:safe");
         String proposePrice = jedis.get("price:propose");
@@ -33,6 +43,44 @@ public class GasService {
         result.setSafeGasPrice(safePrice);
         result.setProposeGasPrice(proposePrice);
         return result;
+    }
+
+    public static JedisPool getPool() {
+        try {
+            TrustManager bogusTrustManager = new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            };
+
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, new TrustManager[]{bogusTrustManager}, new java.security.SecureRandom());
+
+            HostnameVerifier bogusHostnameVerifier = (hostname, session) -> true;
+
+            JedisPoolConfig poolConfig = new JedisPoolConfig();
+            poolConfig.setMaxTotal(10);
+            poolConfig.setMaxIdle(5);
+            poolConfig.setMinIdle(1);
+            poolConfig.setTestOnBorrow(true);
+            poolConfig.setTestOnReturn(true);
+            poolConfig.setTestWhileIdle(true);
+
+            return new JedisPool(poolConfig,
+                    URI.create(System.getenv("REDISCLOUD_URL")),
+                    sslContext.getSocketFactory(),
+                    sslContext.getDefaultSSLParameters(),
+                    bogusHostnameVerifier);
+
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException("Cannot obtain Redis connection!", e);
+        }
     }
 
 }
